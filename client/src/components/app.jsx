6ea89281ad4ee90ed894e95eabbb17f52d0be99d
Tim Z <tjkz87@gmail.com>
2016-08-30 10:17:09 -0700

    added seesionId cookie check in App Componenet constructor

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,8 +1,21 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // get sessionId cookie
+    var sessionId = null;
+    var name = 'sessionId=';
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        sessionId = c.substring(name.length, c.length);
+      }
+    }
     this.state = {
-      signedIn: true
+      signedIn: sessionId ? true : false
     };
   }
   toggleSignIn() {
