7675873299f8ddb823fb887a6d5ffbf80e627d3c
Tim Z <tjkz87@gmail.com>
2017-04-25 20:21:53 +0600

    update routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,12 +3,12 @@ var invoiceController = require('../invoices/invoiceController');
 var pdfController = require('../pdfs/pdfController');
 
 module.exports = function(app, express) {
-  app.post('/api/users/info', userController.getInfo);
-  app.post('/api/users/session', userController.createSession);
+  app.post('/users/info', userController.getInfo);
+  app.post('/users/session', userController.createSession);
 
-  app.get('/api/invoices/query', invoiceController.query);
-  app.get('/api/invoices/querybyid', invoiceController.queryById);
+  app.get('/invoices/query', invoiceController.query);
+  app.get('/invoices/querybyid', invoiceController.queryById);
 
-  app.get('/api/pdfs/order', pdfController.order);
-  app.get('/api/pdfs/download', pdfController.download);
+  app.get('/pdfs/order', pdfController.order);
+  app.get('/pdfs/download', pdfController.download);
 };
