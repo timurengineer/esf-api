014cea2dbca82a880985581638464d75d354d520
Tim Z <tjkz87@gmail.com>
2017-04-25 19:51:45 +0600

    remove frontend code

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import App from './components/app.js';
-
-injectTapEventPlugin();
-
-render(<App />, document.getElementById('app'));
